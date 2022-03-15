36afc5c6ce98da83ecfe0fbfdc64c2689d0ba62b
finnTitus <finn2007nuernberg@gmail.com>
2022-03-15 10:50:10 +0100

    4 Rechenarten

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        float zahl1 = 13; float zahl2 = 11;
+        float zahl1 = 13 ; float zahl2 = 11;
+        System.out.println(zahl1 +  " - " +zahl2 +" = " +sub(zahl1,zahl2));
         System.out.println(zahl1 +  " + " +zahl2 +" = " +add(zahl1,zahl2));
-
-    }
-
+        System.out.println(zahl1 +  " * " +zahl2 +" = " +mul(zahl1,zahl2));
+        System.out.println(zahl1 +  " / " +zahl2 +" = " +div(zahl1,zahl2));
+}
 
 
 
@@ -13,5 +14,21 @@ public class Main {
 
         return zahl1 + zahl2;
     }
-}
 
+
+    public static float sub(float zahl1,float zahl2) {
+
+        return zahl1 - zahl2;
+    }
+
+    public static float mul(float zahl1,float zahl2) {
+
+        return zahl1 * zahl2;
+    }
+
+    public static float div(float zahl1,float zahl2) {
+
+        return zahl1 / zahl2;
+    }
+
+}
