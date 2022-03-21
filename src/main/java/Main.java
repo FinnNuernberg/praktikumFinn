4abe4691d88072755490ea4939b24c094d1feee1
finnTitus <finn2007nuernberg@gmail.com>
2022-03-21 13:53:56 +0100

    Erweiterung der Aufgabe in Konsole MainTest add + sub in einer Aufgabe möglich

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.util.Objects;
 import java.util.Scanner;
 
 
-
 public class Main {
 
     public static void main(String[] args) {
@@ -14,40 +13,71 @@ public class Main {
 
         System.out.println("Guten Tag - Wilkommen im Taschenrechner. Folgende Rechenarten stehen zur Verfügung [add], [sub], [mul], [div]");
         System.out.println("Geben Sie eine Aufgabe an: ");
-        String aufgabe = in.nextLine();
+        String aufgabe = in.nextLine(); //Aufgabe des Nutzers in der Konsole
+        if (args.length != 0) {
+            aufgabe = args[0];
+            for (int i =)
+        }
         String[] zeichen = aufgabe.split(" ");
-        for (int i = 1; i < zeichen.length + 1 ; i = i + 1) {
-            if (i % 2 == 0) {
-                // operanten
-                System.out.println("operant:"+zeichen[i - 1]);
-            } else {
-                // zahl
-                System.out.println("zahl:"+zeichen[i - 1]);
+        berechneErgebnis(zeichen);
+    }
+
+    private static void berechneErgebnis(String[] zeichen) {
+        float zwischenErgebnis = 0;
+        float zahl1 = Float.parseFloat(zeichen[0]);
+        zwischenErgebnis = zahl1;
+        String operant = "";
+        String zahltext = "";
+        for (int i = 1; i < zeichen.length; i = i + 2) {
+
+            operant = zeichen[i]; //zweites zeichen im ersten Schleifendurchlauf
+            zahltext = zeichen[i + 1]; //erstes zeichen im ersten Schleifendurchlauf
+
+
+
+            float zahl = Float.parseFloat(zahltext);
+
+            if (Objects.equals(operant, "+")) {
+                zwischenErgebnis = add(zwischenErgebnis, zahl);
+                System.out.println(zwischenErgebnis);
+            }
+            if (Objects.equals(operant, "-")) {
+                zwischenErgebnis = sub(zwischenErgebnis, zahl);
+                System.out.println(zwischenErgebnis);
+            }
+            if (Objects.equals(operant, "*")) {
+                zwischenErgebnis = mul(zwischenErgebnis, zahl);
+                System.out.println(zwischenErgebnis);
+            }
+            if (Objects.equals(operant, "/")) {
+                zwischenErgebnis = div(zwischenErgebnis, zahl);
+                System.out.println(zwischenErgebnis);
             }
         }
+    }
 
 
-        }
-
-
-
-    public static float add(float zahl1,float zahl2) {
+    public static float add(float zahl1, float zahl2) {
 
         return zahl1 + zahl2;
     }
 
 
-    public static float sub(float zahl1,float zahl2) {
+    public static float sub(float zahl1, float zahl2) {
 
         return zahl1 - zahl2;
     }
 
-    public static float mul(float zahl1,float zahl2) {
+    public static float mul(float zahl1, float zahl2) {
 
         return zahl1 * zahl2;
     }
 
-    public static float div(float zahl1,float zahl2) {
+    public static float div(float zahl1, float zahl2) {
+        if (zahl2 == 0) {
+            System.out.println("Sie dürfen nicht durch 0 teilen");
+            throw new ArithmeticException("Nutzer hat durch 0 geteilt");
+        }
 
         return zahl1 / zahl2;
     }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void main_test() {
+    }
+
+    @Test
+    public void add_test() {
+    float ergebnis = Main.add(18,24);
+        assertEquals(42,ergebnis,0.01);
+    }
+
+    @Test
+    public void sub_test() {
+        float ergebnis = Main.sub(18,24);
+        assertEquals(-6,ergebnis,0.01);
+    }
+
+    @Test
+    public void mul_test() {
+        float ergebnis = Main.mul(8,9);
+        assertEquals(72,ergebnis,0.01);
+
+        ergebnis = Main.mul(-8,9);
+        assertEquals(-72,ergebnis,0.01);
+    }
+
+    @Test (expected = ArithmeticException.class)
+    public void div_test() {
+        float ergebnis = Main.div(5,0);
+
+    }
+}
